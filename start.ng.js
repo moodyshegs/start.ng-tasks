3785300321ba679a4df14dfd8e92d8205a176d92
moodyshegs <61108798+moodyshegs@users.noreply.github.com>
2020-03-26 12:35:41 +0100

    Add files via upload

---
+++ start.ng.js
@@ -0,0 +1,16 @@
+let name = "moody segun"
+ console.log(name)
+
+let course = ['html', 'javascript', 'css', 'java', 'kotlin', 'python']
+ console.log(course)
+
+ for(i=1; i<=200; i++){
+   // let's divide the value by 2
+   // if the remainder is zero then it's an even number
+  
+   if ((course.length % 2 == 0) && (i % 2 == 0)) {
+     console.log(i);
+   } if ((course.length % 2 !== 0) && (i % 2 !== 0)){
+      console.log(i);
+   } 
+ }  
